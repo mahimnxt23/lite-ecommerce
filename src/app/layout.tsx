38408839ba1483373b75ea1cc1c4531835ae4d76
mahim <mahimnxt23@gmail.com>
2025-08-25 08:34:54 +0600

    implemented components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,31 @@
-import type { Metadata } from "next";
-import { Jost } from "next/font/google";
+import type {Metadata} from "next";
+import {Jost} from "next/font/google";
 import "./globals.css";
+import {Footer, Navbar} from "@/components";
+import React from "react";
 
 const jost = Jost({
-  variable: "--font-josh",
-  subsets: ["latin"],
+    variable: "--font-josh",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Lite E-commerce",
-  description: "A lightweight e-commerce site",
+    title: "Lite E-commerce",
+    description: "A lightweight e-commerce site",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${jost.className} antialiased`}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <body className={`${jost.className} antialiased`}>
+        <Navbar/>
+        {children}
+        <Footer/>
+        </body>
+        </html>
+    );
 }
